2f664b3344f021b020a50965da2b04fdcb2535bb
matheus <wallysom2@gmail.com>
2022-12-09 06:36:57 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <v-app>
+   <v-container>
+    <v-card>
+      <v-row>
+        <v-col cols="2" 
+        v-for="pokemon in pokemons.slice(0,10)" 
+        :key="pokemon.name">
+        <v-card>
+          <v-container>
+            <h2 class="text-center"> {{pokemon.name}} </h2>
+          </v-container>
+        </v-card>
+
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+
+ </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import axios from 'axios';
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
+ 
+  },
+
+data (){
+  return {
+    pokemons: []
   }
-}
+},
+
+  mounted (){
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151')
+    .then(response => {
+      this.pokemons = response.data.results;
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+};
+
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: rgb(2,0,36);
+  background: linear-gradient(90deg, rgba(2,0,36,0.9023984593837535) 0%, rgba(9,121,93,1) 80%, rgba(73,88,91,1) 100%);
 }
 </style>
